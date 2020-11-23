9b8497e3845dd6f4319e7a5c2a600d4db04ab3da
Ryan Monaghan <aftereffectsjester@gmail.com>
2020-11-23 14:31:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,33 @@
     <p class="lead">or</p>
     <hr/>
     <h1>Sign Up</h1>
-    
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputFirst">First Name</label>
+        <input name="first" type="text" class="form-control" id="exampleInputFirst" aria-describedby="emailHelp">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputLast">Last Name</label>
+        <input name="last" type="text" class="form-control" id="exampleInputLast" aria-describedby="emailHelp">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputGrad">Graduation Year</label>
+        <input name="last" type="text" class="form-control" id="exampleInputGrad" aria-describedby="emailHelp">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputId">Student Id</label>
+        <input name="last" type="text" class="form-control" id="exampleInputId" aria-describedby="emailHelp">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <button type="submit" class="btn btn-primary">Log In</button>
+    </form>
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
